756b5e237e6f872cbbc0ce8bf855e3e33ad1b1a5
AhsanYousaf <ahsanbhatti624@gmail.com>
2023-01-28 23:31:10 +0500

    Created backend for generating image

--- server/controlers/openaiControler.js
+++ server/controlers/openaiControler.js
@@ -7,11 +7,14 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 const generateImage = async (req, res) => {
+    const { prompt, size } = req.body;
+    const imageSize = size === 'small' ? '256x256' : size === 'medium' ? '512x512' : '1024x1024';
+
     try {
         const response = await openai.createImage({
-            prompt: 'Lion drinking coffee while working on laptop',
+            prompt: prompt,
             n: 1,
-            size: '512x512'
+            size: imageSize
         });
 
         const imageUrl = response.data.data[0].url
